03c667374b47511fb9110718899b661fd4d7fca6
Shehan <dmcshehan@outlook.com>
2020-07-17 23:56:38 +0530

    fix App

--- src/App/App.js
+++ src/App/App.js
@@ -42,7 +42,6 @@ export default function App() {
 
             <Switch>
               <Route exact path='/' component={Home} />
-              <Route exact path='/' component={Home} />
               <Route exact path='/signin' component={Signin} />
               <Route exact path='/dashboard' component={Dashboard} />
               <Route path='/' component={NotFound} />
